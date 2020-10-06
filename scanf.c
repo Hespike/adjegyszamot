fba3e1086d5a2a65c481c178add6708e9cab2041
Hespi <sardi.andras@gmail.com>
2020-10-06 14:24:36 +0200

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	int number;
+	printf("Kerem adjon meg egy egesz szamot!\n");
+	scanf("%d", &number);
+
+	printf("A beolvasott szamod: %d\n", number);
+	return 0;
+}
